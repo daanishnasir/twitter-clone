627171f7135971cee0e6468db129780cb0bda3af
daanishnasir <daanishnasir@gmail.com>
2017-02-17 16:19:21 -0600

    Done

--- js/animations.js
+++ js/animations.js
@@ -1,12 +1,10 @@
-// * As the user types, the character count should decrease.
 
-// * When there are 10 or less characters, the character counter should turn red.
-// * If the user puts in more than 140 characters, the tweet button should be disabled (and re-enabled when there are <= 140 chars).
-// * When the user successfully inputs characters and clicks the “Tweet” button, a new tweet should be created and added to the tweet stream in the main column, using the user’s fake profile image in the top left and username/fullname.
 
 $(document).ready(function(){
 
 $('#tweet-submit, #char-count').hide();
+$('.tweet-actions').hide();
+$('.stats').hide();
 
 
 
@@ -45,24 +43,52 @@ $('.tweet-compose').keyup(function(){
     }
 
 });
+// Tweet button function below
 
 $('#tweet-submit').click(function(){
-  console.log('tweet click is working');
-  $('.dashboard').prependTo('#stream');
+  // console.log('tweet click is working');
+
+  var $tweet = $('.tweet-compose');
+  var $clone = $('#stream').children().first().clone();    //found the first .tweet and cloned it.
+
+  $clone.find('.tweet-text').text($tweet.val());  // in that first tweet find the class .tweet-text (Today is an amazing day) and change/replace its value with the tweet compose value in the clone
+
+  $clone.find('.fullname').text('Chuck Norris');
+  $clone.find('.username').text('@daanishnasir');
+
+
+
+  $clone.find('.avatar').attr('src', 'http://www.americaremembers.com/wp-content/uploads/2013/08/Chuck-Norris-BW.jpg');//find the image (class avatar) and change its attr to a new image, which is the same)
+
+
+  $tweet.val('');   //the last thing happening in this function is changing the value to tweet-compose to blank.
+
+  $('#stream').prepend($clone);   //add on top of stream the new tweet.
+    //Basically we just cloned a tweet and replaced every value of that clone with what we needed instead to make it look like a new one.
 });
 
 
+// ## Part Two
+// * The tweet actions (Reply, Retweet, etc) should only show up when you hover over that individual tweet. Otherwise, they should be hidden.
+// * The Retweets/timestamp/Reply areas should also be hidden by default. These should only expand if you click on the tweet. Have the students use a jQuery animation to accomplish the reveal, similar to how it’s done on Twitter.com
+
+
+// $( selector ).mouseenter( handlerIn ).mouseleave( handlerOut );
 
 
 
+$('#stream').on('mouseenter', '.content', function(){
+  $(this).find('.tweet-actions').show();
+});
+
+$('#stream').on('mouseleave', '.content', function(){
+  $(this).find('.tweet-actions').hide();
+});
 
 
-
-
-
-
-
-
+$('#stream').on('click','.content', function(){
+  $(this).find('.stats').fadeIn('slow');
+});
 
 
 
